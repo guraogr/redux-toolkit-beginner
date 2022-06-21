2a1208aa1080ffa9ff4b7bff8164f1153339b328
guraogr <guraogr@gmail.com>
2022-06-22 00:32:53 +0900

    feat: useDispatchを利用してCountの値を増減する機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './redux/store';
+import { decrease, increase } from './redux/counterSlice';
 
 const App = () => {
   // useSelector: counterSliceで設定したcountの値を取得することができます。
@@ -8,9 +9,19 @@ const App = () => {
   // counterSlice.tsファイルのnameで設定した”counter”ではない。
   const count = useSelector((state: RootState) => state.counter.count);
 
+  // Action Creatorsを実行するためにuseDispatchを利用
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>Count: {count}</h1>
+      {/* dispatchにAction Creatorsを指定 */}
+      <button type="button" onClick={() => dispatch(increase())}>
+        Up
+      </button>
+      <button type="button" onClick={() => dispatch(decrease())}>
+        Down
+      </button>
     </div>
   );
 };
