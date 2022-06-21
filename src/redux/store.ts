131fad30320f5e4c90d872e190eb67906cf2f9d5
guraogr <guraogr@gmail.com>
2022-06-22 00:15:16 +0900

    feat: StoreへのSliceの追加

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
 
 // すべてのコンポーネントからアクセス可能なstoreと呼ばれる場所を作成する
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    // 作成したcounterSliceをimportしてreducerに設定します。
+    counter: counterReducer,
+    // 複数のSliceを作成した場合は作成したSliceをimportしてreducerプロパティのオブジェクトに追加することで
+    // 各Sliceで設定したstateをすべてのコンポーネントからアクセスすることが可能になります。
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
